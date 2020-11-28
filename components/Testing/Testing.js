51ee221f344ff654829bde4327b1c306ef18cfd7
Saruj-chy <saruj.chy222@gmail.com>
2020-11-28 09:36:51 +0600

    convert c drive to f drive in project

---
+++ components/Testing/Testing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Testing = () => {
+  return (
+    <View style={{ backgroundColor: 'green', flex: 1, }}>
+      <Text style={{ padding: 10, fontSize: 18 }}>Hello man</Text>
+      <Text style={{ padding: 10, fontSize: 18 }}>Below man</Text>
+    </View>
+  );
+};
+
+export default Testing;
